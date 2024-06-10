f14cf2d4c577a9930aba43f3f12cafc3293994cd
ChanHyeok <dhrcksgur1@gmail.com>
2024-06-10 20:16:21 +0900

    Test : 산 전체조회 테스트 코드 추가

--- src/test/java/com/example/santa/domain/mountain/service/MountainServiceImplTest.java
+++ src/test/java/com/example/santa/domain/mountain/service/MountainServiceImplTest.java
@@ -15,7 +15,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -61,9 +65,23 @@ public class MountainServiceImplTest {
         pageable = mock(Pageable.class);
     }
 
-
     @Test
-    void findMountainById_returnsMountainDto() {
+    void findAllMountains_Success() {
+        List<Mountain> mountainList = List.of(mountain);
+        Page<Mountain> mountainPage = new PageImpl<>(mountainList);
+        when(mountainRepository.findAll(pageable)).thenReturn(mountainPage);
+        when(mountainResponseDtoMapper.toDto(mountain)).thenReturn(dto);
+
+        Page<MountainResponseDto> result = mountainService.findAllMountains(pageable);
+
+        assertNotNull(result);
+        assertEquals(1, result.getTotalElements());
+        assertEquals(dto, result.getContent().get(0));
+        verify(mountainRepository).findAll(pageable);
+        verify(mountainResponseDtoMapper).toDto(mountain);
+    }
+    @Test
+    void findMountainById_Success() {
         when(mountainRepository.findById(anyLong())).thenReturn(Optional.of(mountain));
         when(mountainResponseDtoMapper.toDto(mountain)).thenReturn(dto);
 
@@ -76,7 +94,7 @@ public class MountainServiceImplTest {
     }
 
     @Test
-    void findMountainById_throwsExceptionWhenNotFound() {
+    void findMountainById_ThrowsExceptionIfNotFound() {
         when(mountainRepository.findById(anyLong())).thenReturn(Optional.empty());
 
         assertThrows(ServiceLogicException.class, () -> mountainService.findMountainById(anyLong()));
